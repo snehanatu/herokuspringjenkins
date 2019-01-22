267d163575e2bbe9ca57456a3ed3985f8c830133
monali mayaskar <snehanatu@yahoo.com>
2019-01-22 09:55:58 -0500

    Initial Commit

---
+++ src/main/java/com/techtalentsouth/herokuDemo/HerokuDemoApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.herokuDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/techtalentsouth/herokuDemo/HerokuDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.herokuDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokuDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
